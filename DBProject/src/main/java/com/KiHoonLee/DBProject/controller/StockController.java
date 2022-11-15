99db030fe54b2213e3defdf5faafad480d2626f4
HanSeoJin <yp6484@gmail.com>
2022-11-16 01:18:55 +0900

    [추가] 국내 관심종목 구현

--- DBProject/src/main/java/com/KiHoonLee/DBProject/controller/StockController.java
+++ DBProject/src/main/java/com/KiHoonLee/DBProject/controller/StockController.java
@@ -1,4 +1,24 @@
 package com.KiHoonLee.DBProject.controller;
 
+import com.KiHoonLee.DBProject.repository.StockRepository;
+import com.KiHoonLee.DBProject.table.StockNamePriceChange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class StockController {
+    @Autowired
+    private StockRepository stockRepository;
+
+    //국내 관심 주식리스트
+    @GetMapping("/stock/interest")
+    public ResponseEntity<?> getStockInterestList() {
+        List<StockNamePriceChange> stockNamePriceChanges = stockRepository.findAllStockInterest();
+        return new ResponseEntity<>(stockNamePriceChanges, HttpStatus.OK);
+    }
 }
--- DBProject/src/main/java/com/KiHoonLee/DBProject/repository/StockRepository.java
+++ DBProject/src/main/java/com/KiHoonLee/DBProject/repository/StockRepository.java
@@ -1,4 +1,29 @@
 package com.KiHoonLee.DBProject.repository;
 
+import com.KiHoonLee.DBProject.table.StockNamePriceChange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class StockRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    //국내탭에서 관심종목 리스트를 얻음
+    //관심종목 중 급락비 기준
+    public List<StockNamePriceChange> findAllStockInterest() {
+        var rowMapper = BeanPropertyRowMapper.newInstance(StockNamePriceChange.class);
+        List<StockNamePriceChange> stockNamePriceChanges = jdbcTemplate.query(
+                "SELECT stk.STK_NM, S.S_LAST, S.S_CHG\n" +
+                "FROM WATCHLIST as W, STOCK_QUOTE as S, STOCK as stk\n" +
+                "WHERE Date(S_DATE) = '2022-10-28' AND W.STOCK_STK_CD = stk.STK_CD AND stk.STK_CD = S.STOCK_STK_CD\n" +
+                "GROUP BY W.STOCK_STK_CD, S.S_LAST, S.S_CHG\n" +
+                "ORDER BY S.S_CHG DESC"
+                ,rowMapper);
+        return stockNamePriceChanges;
+    }
 }
