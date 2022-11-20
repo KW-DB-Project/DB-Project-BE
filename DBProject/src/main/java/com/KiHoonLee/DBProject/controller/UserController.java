df5881428ea2962e00c46adbce56fe1c6ca7b214
HanSeoJin <yp6484@gmail.com>
2022-11-20 16:16:23 +0900

    [수정] user api주소 수정

--- DBProject/src/main/java/com/KiHoonLee/DBProject/controller/UserController.java
+++ DBProject/src/main/java/com/KiHoonLee/DBProject/controller/UserController.java
@@ -9,16 +9,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
 
     //로그인
-    @PostMapping("api/user/login")
+    @PostMapping("login")
     public ResponseEntity<?> authorizeUser(@RequestBody IdPassword idPassword) {
         return new ResponseEntity<>(userService.isMember(idPassword), HttpStatus.OK);
     }
