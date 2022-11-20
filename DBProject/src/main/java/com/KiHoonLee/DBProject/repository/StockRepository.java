dd1a596edf7587203eb3765a669f8d9da4fe27f5
kangholim <55732952+kangholim@users.noreply.github.com>
2022-11-20 18:21:47 +0900

    [수정] 쿼리문
    나이. 거래량, 업종별 관심순위

--- DBProject/src/main/java/com/KiHoonLee/DBProject/repository/StockRepository.java
+++ DBProject/src/main/java/com/KiHoonLee/DBProject/repository/StockRepository.java
@@ -20,40 +20,31 @@ public class StockRepository {
     public List<AgeRankingDto> findAgeRaking() {
         var rowMapper = BeanPropertyRowMapper.newInstance(AgeRankingDto.class);
         List<AgeRankingDto> ageRankingDto = jdbcTemplate.query("select  case\n" +
-                "\t\twhen u.age between 10 and 19 then '10대'\n" +
-                "        when u.age between 20 and 29 then '20대'\n" +
-                "        when u.age between 30 and 39 then '30대'\n" +
-                "        when u.age between 40 and 49 then '40대'\n" +
-                "        else '50대 이상'\n" +
-                "        end as age_group,\n" +
-                "\t\ts.STK_NM, q.S_LAST, count(w.STOCK_STK_CD) as cnt\n" +
+                "      when u.age between 10 and 19 then '10대'\n" +
+                "      when u.age between 20 and 29 then '20대'\n" +
+                "      when u.age between 30 and 39 then '30대'\n" +
+                "      when u.age between 40 and 49 then '40대'\n" +
+                "      else '50대 이상'\n" +
+                "      end as age_group,s.STK_NM, q.S_LAST, count(w.STOCK_STK_CD) as cnt\n" +
                 "from s_user u, watchlist w, stock_quote q , stock s\n" +
                 "where u.id = w.s_user_id and q.STOCK_STK_CD = s.STK_CD and s.STK_CD = w.STOCK_STK_CD and Date(s_date)='2022-10-28'\n" +
-                "group by w.stock_stk_cd, u.AGE div 10\n" +
-                "order by age_group, count(w.STOCK_STK_CD) desc\n" +
-                "\n",rowMapper);
+                "group by w.stock_stk_cd, age_group,s.STK_NM, q.S_LAST\n" +
+                "order by age_group, count(w.STOCK_STK_CD) desc",rowMapper);
         return ageRankingDto;
     }
 
-    public List<AmountRankingDto> findAmountRaking() {
+    public List<AmountRankingDto> findAmountRaking() { //
         var rowMapper = BeanPropertyRowMapper.newInstance(AmountRankingDto.class);
         List<AmountRankingDto> amountRankingDto = jdbcTemplate.query("select  case\n" +
-                "\t\twhen q.S_LAST between 10000 and 49999 then '만원대'\n" +
-                "        when q.S_LAST between 50000 and 99999 then '오만원대'\n" +
-                "        when q.S_LAST between 100000 and 499999 then '십만원대'\n" +
-                "        when q.S_LAST between 500000 and 1000000 then '오십만원대'\n" +
-                "        end as money_group,\n" +
-                "\t\ts.STK_NM, q.S_LAST, count(w.STOCK_STK_CD) as cnt\n" +
+                "      when q.S_LAST between 10000 and 49999 then '만원대'\n" +
+                "      when q.S_LAST between 50000 and 99999 then '오만원대'\n" +
+                "      when q.S_LAST between 100000 and 499999 then '십만원대'\n" +
+                "      when q.S_LAST between 500000 and 1000000 then '오십만원대'\n" +
+                "      end as money_group, s.STK_NM, q.S_LAST, count(w.STOCK_STK_CD) as cnt\n" +
                 "from s_user u, watchlist w, stock_quote q , stock s\n" +
                 "where u.id = w.s_user_id and q.STOCK_STK_CD = s.STK_CD and s.STK_CD = w.STOCK_STK_CD and Date(s_date)='2022-10-28'\n" +
-                "group by w.stock_stk_cd, case\n" +
-                "\t\twhen q.S_LAST between 10000 and 49999 then '만원대'\n" +
-                "        when q.S_LAST between 50000 and 99999 then '오만원대'\n" +
-                "        when q.S_LAST between 100000 and 499999 then '십만원대'\n" +
-                "        when q.S_LAST between 500000 and 1000000 then '오십만원대'\n" +
-                "        end \n" +
-                "order by money_group, count(w.STOCK_STK_CD) desc\n" +
-                "\n",rowMapper);
+                "group by w.stock_stk_cd, money_group, s.STK_NM, q.S_LAST\n" +
+                "order by money_group, count(w.STOCK_STK_CD) desc",rowMapper);
         return amountRankingDto;
     }
 
@@ -63,7 +54,7 @@ public class StockRepository {
                 "from enterprise_info e, s_user u, watchlist w, stock_quote q , stock s\n" +
                 "where u.id = w.s_user_id and q.STOCK_STK_CD = s.STK_CD and s.STK_CD = w.STOCK_STK_CD and s.STK_CD = e.STOCK_STK_CD \n" +
                 "and Date(s_date)='2022-10-28'\n" +
-                "group by e.CATEGORY, w.stock_stk_cd\n" +
+                "group by e.CATEGORY, w.stock_stk_cd, s.STK_NM, q.S_LAST\n" +
                 "order by e.CATEGORY asc, count(w.STOCK_STK_CD) desc\n" +
                 "\n" ,rowMapper);
         return sectorRankingDto;
