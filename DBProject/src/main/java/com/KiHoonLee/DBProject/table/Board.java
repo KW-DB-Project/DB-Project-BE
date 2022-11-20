e27d657b8b9a7f8ffa936c0a8af7fa1d53aa0165
HanSeoJin <yp6484@gmail.com>
2022-11-20 15:02:27 +0900

    [수정] Board 필드명 수정

--- DBProject/src/main/java/com/KiHoonLee/DBProject/table/Board.java
+++ DBProject/src/main/java/com/KiHoonLee/DBProject/table/Board.java
@@ -3,6 +3,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -14,6 +17,6 @@ public class Board {
     private String stockStkCd;
     private String title;
     private String content;
-    private Date creatDate;
+    private Date createDate;
     private int bLike;
 }
